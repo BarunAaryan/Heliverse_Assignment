e6d72490fa42d6fa71810a5699ef9489eb3b7259
Barun Aaryan <barunaryan1@gmail.com>
2024-03-31 17:25:33 +0530

    Done with project

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import './App.css'
+import './index.css'
+
+import UsersList from "./Components/UserList";
+
+function App() {
+  return (
+    <div className="App">
+      <UsersList/>
+    </div>
+  );
+}
+
+export default App;
